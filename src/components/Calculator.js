8905b7db89f8779402597cf49898cae5ef239011
simooooone <s.foschi@protonmail.com>
2020-08-28 13:11:44 +0200

    Hiding history component if there is no text inside

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,4 +1,4 @@
-import React, { useState /* useEffect */ } from "react";
+import React, { useState } from "react";
 import ItemButton from "./Button";
 import Display from "./Display";
 import History from "./History";
--- src/components/History.js
+++ src/components/History.js
@@ -1,14 +1,23 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useRef } from "react";
 
 const History = (params) => {
+  let renderHistory = useRef(null);
   // Set the focus for the last element in history scrolling the div at bottom
   useEffect(() => {
     let historyInternal = document.querySelector(".history-internal");
     historyInternal.scrollTop = historyInternal.scrollHeight;
   }, [params]);
 
+  useEffect(() => {
+    if (params.ret !== "") {
+      renderHistory.current.style = "display: block";
+    } else {
+      renderHistory.current.style = "display: none";
+    }
+  }, [params.ret]);
+
   return (
-    <div className="history">
+    <div className="history" ref={renderHistory}>
       <div
         className="history-internal"
         dangerouslySetInnerHTML={{ __html: params.ret }}
